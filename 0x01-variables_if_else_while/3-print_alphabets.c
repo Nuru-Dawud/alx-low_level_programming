f9c9a701af3bb6e3e0cc63867da2b1713a78ff50
nurudawud2@gmail.com <nurudawud2@gmail.com>
2022-04-09 01:15:36 -0700

    3-commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,8 +14,8 @@ int main(void)
 		putchar(low);
 	for (low = 'A'; low <= 'Z'; low++)
 	{
-	        putchar(low);
+	         putchar(low);
 	}
-	        putchar('\n');
+	         putchar('\n');
 	return (0);
 }
