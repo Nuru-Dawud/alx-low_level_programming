209b1da21b36824ff436e9f79a6a12b8e76a3a4f
nurudawud2@gmail.com <nurudawud2@gmail.com>
2022-04-13 05:51:01 -0700

    0-putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ char str[] = "_putchar\n";
 int i = 0;
 
 while (str[i] != '\0')
-        {
-        char c = str[i];
+	{
+		char c = str[i];
 
-        _putchar(c);
-        i++;
-        }
+		_putchar(c);
+		i++;
+	}
 return (0);
 }
