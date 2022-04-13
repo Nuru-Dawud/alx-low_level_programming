9bd410fbee294ea0a00e93c11db0b8e806752a35
nurudawud2@gmail.com <nurudawud2@gmail.com>
2022-04-13 05:05:36 -0700

    putchar  commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
- /*
+/*
  *  This is the description of main
- */
- 
-int main()
+ */ 
+int main(void)
 {
-  
 /* 
  *  Get the character to be written
  *  
  */
-    	char h[] = "_putchar";
+	char h[] = "_putchar";
 	int m;
-
 	m = 0;
-
 	while (m <= 8)
 	{
 		putchar(h[m]);
 		m++;
 	}
-	putchar('\n');
-  
+	putchar('\n');  
     return (0);
 }
