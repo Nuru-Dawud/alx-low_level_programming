1041bfff230e218f0286397065ad0b005089deaf
Nuru-Dawud <80962071+Nuru-Dawud@users.noreply.github.com>
2022-07-08 19:57:18 +0300

    Update 102-result.c

--- 0x17-doubly_linked_lists/102-result.c
+++ 0x17-doubly_linked_lists/102-result.c
@@ -1,4 +1,13 @@
-#include <lists.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ *
+ * @head: the head of the doubly linked list
+ * @n: the value of the node to add
+ *
+ * Return: the new pointer
+ */
 
 n = 0
 for a in range(9999, 100, -1):
